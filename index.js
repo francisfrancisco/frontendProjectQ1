1247fdef05eccb1b4184d49484cb410c28b59e02
Francis Francisco <francis.francisco44@gmail.com>
2018-08-29 19:53:10 -0700

    refactored some JS

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 console.log("LETS GO KNICKS")
 // TODOLIST
 const lis = document.querySelectorAll("li");
-const newTask = document.querySelector("#task");
+const task = document.querySelector("#task");
+const list = document.querySelector("#list")
 
-newTask.addEventListener("change", (e) => {
+task.addEventListener("change", (e) => {
  e.preventDefault();
- document.querySelector("ul").innerHTML += `<li>${newTask.value}</li>`;
- newTask.value = "";
+ document.querySelector("ul").innerHTML += `<li>${task.value}</li>`;
+ task.value = "";
 })
 
 list.addEventListener('click', (e) => {
+  e.target.classList.toggle("done");
+});
+list.addEventListener("mouseover", (e) => {
   e.target.classList.add("selected");
-})
+});
+list.addEventListener("mouseout", (e) => {
+  e.target.classList.remove("selected");
+});
+list.addEventListener("dblclick", (e) => {
+  list.removeChild(e.target);
+});
 
 //EVENT FOR NAME CHANGE
 const userName = document.querySelector("#user");
 
 userName.addEventListener("change", () => {
-  document.querySelector("#greeting").innerHTML = `<p>Greetings, ${userName.value}</p>`
+  document.querySelector("#greeting").innerHTML = `
+  <p>Greetings, ${userName.value}</p>
+  `
 })
 
+const api = 'yxiX3reOVJnwyOsUaAIQ0zmke2k4YP6L'
+
 //AJAX REQUESTS
-// document.addEventListener("DOMContentLoaded", () => {
-//   axios.get(``)
-//       .then((response) => {
-//           console.log(response)
-//       })
-// })
+document.addEventListener("DOMContentLoaded", () => {
+  axios.get("api.giphy.com/v1/gifs/random?api_key=yxiX3reOVJnwyOsUaAIQ0zmke2k4YP6L")
+      .then((response) => {
+          console.log(response.data)
+      }).catch(error => console.log(error))
+  axios.get("api.openweathermap.org/data/2.5/weather?q=phoenix&APPID=1ea5c5d3585bd190c69d54fd139d6ddd")
+      .then((response) => {
+          console.log(response)
+      })
+      .catch(error => console.log(error))
+})
 
 //CLOCK
 function clock() {
